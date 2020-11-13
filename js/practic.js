21eaca50a6acd82485155140729781332678a4f9
Vera Korzun <veraminich@gmail.com>
2020-11-13 23:48:13 +0100

    Добавляет 1-2 задачу

---
+++ js/practic.js
@@ -0,0 +1,45 @@
+"use strict";
+//=================================================================
+// function getCart(name, age, status, order, price, total) {
+//   console.log(getCart.length); // количество параметров
+//   console.log(arguments.length); // сколько аргументов переданно
+// }
+// getCart("Mick", "Sveta");
+
+//=================================================================
+// function getParams(name, age, ...rest) {
+//   //rest собирает все в массив
+//   console.log(name);
+//   console.log(age);
+//   //console.log(rest);
+//   console.log(Array.isArray(rest)); //проверка на массив
+//   console.log(typeof rest); // тип объект
+// }
+// //getParams("Vova", 20);
+// //getParams("Vova", 20, "status");
+// getParams("Vova", 20, "order", 451);
+
+//=================================================================
+// let janres = ["rap", "rock", "pop"];
+
+// janres.push("jazz");
+
+// console.log(janres);
+
+// function customPush(array, ...item) {
+//   if (!Array.isArray(array)) {
+//     console.log("Передайте массив");
+//     return;
+//   }
+//   let newArray = [...array, ...item];
+//   console.log("typeof", typeof newArray);
+//   return newArray;
+// }
+// // customPush(["rap", "rock", "pop"], "jazz");
+// // console.log(customPush(["rap", "rock", "pop"], "jazz", "heavy metal"));
+// customPush(123456, 123);
+// console.log(customPush(["rap", "rock", "pop"], "jazz", "heavy metal"));
+
+//=================================================================
+//переписать через цикл
+//https://devdocs.io/javascript/
---
+++ js/task-1.js
@@ -0,0 +1,32 @@
+"use strict";
+let message = "";
+const user = {
+  age: 20,
+  hobby: "html",
+  name: "Mango",
+  premium: true,
+};
+
+user["mood"] = "happy"; // добавляет поле 'mood' со значением 'happy'
+user["full time"] = true; // добавляет поле 'full time' со значением true
+user.hobby = "skydiving"; // заменяет значение 'hobby' на 'skydiving'
+user.premium = false; // заменяет значение 'premium' на false
+
+const keys = Object.keys(user);
+for (let key of keys) {
+  message += key + ":" + user[key] + "\n";
+} // в переменную message записывает содержимое объекта user:
+// для переменной keys присваивает массив свойств объекта, используя метод Object.keys()
+// с помощью оператора for...of
+// в формате ключ:значение
+// c переносом строки ('\n')
+
+console.log(message);
+/*
+'age : 20 
+hobby : skydiving 
+name : Mango 
+premium : false 
+mood : happy 
+"full time" : true 
+' */
